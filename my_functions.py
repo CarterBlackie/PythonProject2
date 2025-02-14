53c74c0790a6c3666a9c94f3ef5c950eddec1923
Carter Blackie <carterblackie55@gmail.com>
2025-02-14 18:14:21 -0500

    Code Commenting
    Commenting Code for easier understanding

--- my_functions.py
+++ my_functions.py
@@ -1,3 +1,4 @@
+# Function to return the day of the week based on the given number (1-7)
 def day_of_week(day_number):
     if day_number == 1: 
         return 'Sunday'
@@ -14,11 +15,12 @@ def day_of_week(day_number):
     elif day_number == 7: 
         return 'Saturday'
     else:
-        return 'Error'
+        return 'Error'  # Return error if the input is out of range
 
+# Function to determine air quality based on the Air Quality Index (AQI)
 def air_quality(air_quality_index):
     if air_quality_index < 0: 
-        return 'Error'
+        return 'Error'  # Negative values are invalid
     elif air_quality_index <= 50:
         return 'Good'
     elif air_quality_index <= 100:
@@ -30,42 +32,47 @@ def air_quality(air_quality_index):
     elif air_quality_index <= 300:
         return 'Very Unhealthy'
     else: 
-        return 'Hazardous'
+        return 'Hazardous'  # AQI above 300 is hazardous
 
+# Function to find the largest product of two numbers from three given values
 def largest_product(value1, value2, value3):
     if value1 < value2 and value1 < value3:
-        return value2 * value3
+        return value2 * value3  # Smallest number is value1, multiply the other two
     elif value2 < value3:
-        return value1 * value3
+        return value1 * value3  # Smallest number is value2, multiply value1 and value3
     else:
-        return value1 * value2
-    
-    
+        return value1 * value2  # Smallest number is value3, multiply value1 and value2
+
+# Function to mix two primary RGB colors and return the resulting color
 def colour_mixer(rgb_colour1, rgb_colour2):
-    rgb_colour1 = rgb_colour1.lower()
+    rgb_colour1 = rgb_colour1.lower()  # Convert input to lowercase for consistency
     rgb_colour2 = rgb_colour2.lower()
-    
+
+    # If both colors are the same, return that color
     if rgb_colour1 == rgb_colour2:
         return rgb_colour1
+    # Mixing primary colors
     elif (rgb_colour1, rgb_colour2) in [('red', 'blue'), ('blue', 'red')]:
-        return 'fuchsia'
+        return 'fuchsia'  # Red + Blue = Fuchsia
     elif (rgb_colour1, rgb_colour2) in [('red', 'green'), ('green', 'red')]:
-        return 'yellow'
+        return 'yellow'   # Red + Green = Yellow
     elif (rgb_colour1, rgb_colour2) in [('green', 'blue'), ('blue', 'green')]:
-        return 'aqua'
+        return 'aqua'     # Green + Blue = Aqua
     else:
-        return 'Error'
+        return 'Error'  # Invalid color combination
 
+# Function that returns a phrase based on divisibility by 3 and/or 7
 def yee_ha(number):
     if number % 3 == 0 and number % 7 == 0: 
-        return 'Yee Ha'
+        return 'Yee Ha'  # Divisible by both 3 and 7
     elif number % 7 == 0: 
-        return 'Ha'
+        return 'Ha'  # Divisible only by 7
     elif number % 3 == 0: 
-        return 'Yee'
+        return 'Yee'  # Divisible only by 3
     else: 
-        return 'Nada'
-    
+        return 'Nada'  # Not divisible by either 3 or 7
+
+# Function to display the menu options
 def display_menu():
     print("\nMenu:")
     print("1 - Day of the Week")
